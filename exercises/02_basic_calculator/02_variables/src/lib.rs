f886601b48b2430f079a27a906862aa871a430cb
Rodrigo Lorenzo Leal <code@rlorenzoleal.com>
2024-08-03 16:09:37 +0200

    Solved chapter 2, exercise 2

--- exercises/02_basic_calculator/02_variables/src/lib.rs
+++ exercises/02_basic_calculator/02_variables/src/lib.rs
@@ -9,6 +9,10 @@ pub fn speed(start: u32, end: u32, time_elapsed: u32) -> u32 {
     // TODO: define a variable named `distance` with the right value to get tests to pass
     //  Do you need to annotate the type of `distance`? Why or why not?
 
+    // No need to annotate the type, since start and end are u32, type will be
+    // inferred
+    let distance = end - start;
+
     // Don't change the line below
     distance / time_elapsed
 }
